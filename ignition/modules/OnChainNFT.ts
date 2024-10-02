7b8206ce622dbfcfbbfaede5a116e5569989b5f6
petnacode <peternnadi1999@gmail.com>
2024-10-02 01:09:28 +0100

    deployed the contract

---
+++ ignition/modules/OnChainNFT.ts
@@ -0,0 +1,15 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+
+const OnChainNFTModule = buildModule("OnChainNFTModule", (m) => {
+
+
+  const onChainNFT = m.contract("OnChainNFT");
+
+  return { onChainNFT };
+});
+
+export default OnChainNFTModule;
+
+
